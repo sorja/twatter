88ba2e364beb1698b7feba53075b60168ef08bfe
sorja <miro.sorja@gmail.com>
2016-09-13 00:42:06 +0300

    minor starat

---
+++ __init__.py
@@ -0,0 +1,27 @@
+from flask import Flask, session, redirect, url_for, escape, request, render_template
+import pprint
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        username = session['username']
+        return render_template('index.html', username=username)
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+# set the secret key.  keep this really secret:
+app.secret_key = 'not_in_the_commit'
