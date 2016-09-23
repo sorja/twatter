e6974e2807b50e0adf31ff2b2c969d07336efc3f
sorja <miro.sorja@gmail.com>
2016-09-23 10:07:02 +0300

    typo

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -5,7 +5,7 @@ INSERT INTO users ("full_name", "email", "password") VALUES ('Sami Niemi', 'sami
 
 INSERT INTO users ("full_name", "email", "password") VALUES ('Kalle Niemi', 'kalle.niemi@email.com', 'passw0rd');
 
-INSERT INTO users ("who_id", "whom_id") VALUES (2, 1);
+INSERT INTO follower ("who_id", "whom_id") VALUES (2, 1);
 
 INSERT INTO twaat ("user_id", text) VALUES (1, 'I am the first twaat, hello world');
 INSERT INTO twaat ("user_id", text) VALUES (2, 'Lorem ipsum dolor sit amet, consect');
