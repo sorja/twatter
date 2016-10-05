eed8c5ed99b94652cf8fe7964e6884d76e515f5f
sorja <miro.sorja@gmail.com>
2016-10-05 15:15:23 +0300

    minor event handlers and fucntions to close modal(s)

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -5,3 +5,32 @@ function post_favorite_twaat(twaat) {
   request.send()
 }
 
+function changeAvatar (e){
+  e.preventDefault()
+  var modal = document.getElementById("upload_file");
+  modal.className.split().map(function(e){ console.log(e)})
+  modal.className += ' is-active'; // Click on the checkbox
+}
+
+function closeModal(e){
+  for (x of document.getElementsByClassName('modal')){
+    x.className = "modal";
+  }
+}
+
+document.getElementById("change_avatar").addEventListener("click", changeAvatar);
+
+/* Looks like getElementsByClassName returns a NODE __ LIST which obviously isnt AN ARRAY but a LIST -_- 
+  so, cant map it or do nicely anything :c
+*/
+
+
+// for (x of document.getElementsByClassName("modal-background")){
+//   x.addEventListener("click", closeModal);
+// }
+// for (x of document.getElementsByClassName("modal-close")){
+//   x.addEventListener("click", closeModal);
+// }
+
+document.getElementsByClassName("modal-background")[0].addEventListener("click", closeModal);
+document.getElementsByClassName("modal-close")[0].addEventListener("click", closeModal);
