a60c83060f90cbfb5346bf0883ab7c329b342c68
sorja <miro.sorja@gmail.com>
2016-10-05 15:14:05 +0300

    (this should not be committed in real projects) added some values for allowed file types and size and upload folder

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
 import random
 import string
+import os
 db = dict(
-    db_string = "dbname=sorja user=sorja"
+    db_string = os.environ['DB_STRING']
 )
 
 app = dict(
     secret_key = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(64)),
-    debug = True
+    debug = True,
+    max_content_length= 16 * 1024 * 1024,
+    allowed_extenstions = set(['png', 'jpg', 'jpeg', 'gif']),
+    upload_folder = os.environ['UPLOAD_FOLDER']
 )
